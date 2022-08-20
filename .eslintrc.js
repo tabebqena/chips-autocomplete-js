5efed538949e94d03e80d0ce5664fde7e9b8a845
tabebqena@gmail.com <tabebqena@gmail.com>
2022-08-20 13:35:26 +0300

    stick to es5 (2009), by es5 plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true
+    browser: true
   },
-  extends: 'standard',
+  extends: [
+    "eslint:recommended",
+    "plugin:es5/no-es2015"
+  ],
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
   rules: {
-  }
+
+    "es5/no-arrow-functions": "error"
+
+  },
+  "plugins": [
+    "es5"
+  ]
 }
